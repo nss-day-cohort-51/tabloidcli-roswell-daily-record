fde056cfba5b30ac7c6419345bf8e9ee5578b934
Ben Mosley <bmosley1132@gmail.com>
2021-12-08 15:31:26 -0600

    remove journal entry

--- TabloidCLI/Repositories/JournalRepository.cs
+++ TabloidCLI/Repositories/JournalRepository.cs
@@ -46,7 +46,17 @@ namespace TabloidCLI
         }
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM Journal WHERE Id =@Id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
         public void Update(Journal journal)
         {
