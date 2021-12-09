9152ca5d88933e4eef1930d930feac304d4f5a53
scrawleyy1 <crawley.sydney@gmail.com>
2021-12-09 13:46:08 -0800

    you can now remove and edit tags

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -34,7 +34,7 @@ namespace TabloidCLI.UserInterfaceManagers
                     List();
                     return this;
                 case "2":
-                    Add();
+                    Choose();
                     return this;
                 case "3":
                     Edit();
@@ -58,6 +58,36 @@ namespace TabloidCLI.UserInterfaceManagers
                 Console.WriteLine($"Name: {tag.Name}");
             }
         }
+        private Tag Choose(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose a Tag:";
+            }
+
+            Console.WriteLine(prompt);
+
+            List<Tag> tags = _tagRepository.GetAll();
+
+            for (int i = 0; i < tags.Count; i++)
+            {
+                Tag tag = tags[i];
+                Console.WriteLine($" {i + 1}) {tag.Name}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return tags[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
 
         private void Add()
         {
@@ -70,14 +100,34 @@ namespace TabloidCLI.UserInterfaceManagers
             _tagRepository.Insert(tag);
         }
 
+
         private void Edit()
         {
-            throw new NotImplementedException();
+           Tag tagToEdit = Choose("Which tag would you like to edit?");
+            if (tagToEdit == null)
+            {
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Name (blank to leave unchanged: ");
+            string content = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                tagToEdit.Name = content;
+            }
+            _tagRepository.Update(tagToEdit);
         }
+
+       
 
         private void Remove()
         {
-            throw new NotImplementedException();
+            Tag tagToDelete = Choose("Which tag would you like to remove?");
+            if (tagToDelete != null)
+            {
+                _tagRepository.Delete(tagToDelete.Id);
+            }
         }
     }
 }
