5e3fc469c7367cfa35be0cc91dbb874d67dec8df
Devery Page <pagedevery@gmail.com>
2021-12-10 10:35:21 -0600

    added remove tag feature

--- TabloidCLI/Repositories/PostRepository.cs
+++ TabloidCLI/Repositories/PostRepository.cs
@@ -187,6 +187,24 @@ namespace TabloidCLI.Repositories
             }
         }
 
+        public void DeleteTag(int postId, int tagId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM PostTag 
+                                         WHERE PostId = @postid AND 
+                                               TagId = @tagId";
+                    cmd.Parameters.AddWithValue("@postId", postId);
+                    cmd.Parameters.AddWithValue("@tagId", tagId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
         public void Update(Post post)
         {
             using (SqlConnection conn = Connection)
